e6bb13228b8bdd68613df3ccb6b010b9c82173c6
Omar Tharwat <omartharwat518@gmail.com>
2022-05-11 18:32:04 +0200

    Initial Commit

---
+++ NewWordProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewWordProject
+//
+//  Created by Omar Tharwat on 5/11/22.
+//  Copyright © 2022 Omar Tharwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
